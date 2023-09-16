bed18687f2c60bb03f14ebfcabd0925993341ffa
Ahmed mashur <a.mashur@seamlabs.com>
2023-09-16 23:14:19 +0300

    Java Spring Boot JWT Security

---
+++ src/main/java/com/JWTT/demo/config/JwtService.java
@@ -0,0 +1,66 @@
+package com.JWTT.demo.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.*;
+import java.util.function.Function;
+
+
+@Service
+public class JwtService {
+    private static final String SECRET_KEY = "41E112A25CC228788A8776D7FC24E3C3B13758FFE31C94FE4EB21C5DFC";
+
+    public String extractEmail(String token) {
+
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + (100 * 60 * 24)))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractEmail(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+
+
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> ClaimsResolver) {
+        final Claims claims = extractsAllClaims(token);
+        return ClaimsResolver.apply(claims);
+    }
+
+    private Claims extractsAllClaims(String token) {
+        return (Claims) Jwts.parserBuilder().setSigningKey(getSignInKey())
+                .build().parse(token).getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
---
+++ src/main/java/com/JWTT/demo/user/Role.java
@@ -0,0 +1,6 @@
+package com.JWTT.demo.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
